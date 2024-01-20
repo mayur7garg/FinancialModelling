8a0270ba555f6e5c8edcc6576142aeab439542e6
Mayur Garg <mayur7garg@gmail.com>
2024-01-20 16:30:12 +0000

    Added target columns

--- Stock Forecasting/constants.py
+++ Stock Forecasting/constants.py
@@ -5,3 +5,6 @@ print(f"{NSE_DATA_DIR = } | Valid: {NSE_DATA_DIR.exists() & NSE_DATA_DIR.is_dir(
 
 PROCESSED_DATA_DIR = Path("..", "data", "processed")
 print(f"{PROCESSED_DATA_DIR = } | Valid: {PROCESSED_DATA_DIR.exists() & PROCESSED_DATA_DIR.is_dir()}")
+
+ROLLING_WINDOWS = [3, 7, 15, 30]
+TARGET_WINDOWS = [3, 7, 15, 30]
