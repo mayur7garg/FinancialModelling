bd4b9c3c36a0df31941c352e463a65406e8bb000
Mayur Garg <mayur7garg@gmail.com>
2024-08-27 15:47:18 +0000

    Updated templates

--- Stock Forecasting/templates.py
+++ Stock Forecasting/templates.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from data import StockSummary, StockData
+from data_process import StockSummary, StockData
 
 def create_index(
     template_path: Path,
@@ -35,7 +35,15 @@ def create_stock_report(
     with template_path.open('r', encoding = "utf-8") as f:
         report = f.read()
 
-    report = report.format(symbol = stock_data.symbol)
+    last_candle = "Green" if stock_data.last_candle == 1 else "Red"
+
+    report = report.format(
+        symbol = stock_data.symbol,
+        last_candle = last_candle,
+        last_candle_color = last_candle.lower(),
+        candle_streak = stock_data.candle_streak,
+        streak_cont_prob = f"{stock_data.streak_cont_prob:.1%}"
+    )
 
     with page_out_path.joinpath(f"{stock_data.symbol}.html").open('w', encoding = 'utf-8') as f:
         f.write(report)
